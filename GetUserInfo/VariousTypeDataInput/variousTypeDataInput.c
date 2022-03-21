61efb2d729f748425dd61f4fa2b41eeeb8d9fd96
Md Masum Billah <masum@deligram.com>
2022-03-21 09:35:34 +0600

    Get Various Data Input

---
+++ GetUserInfo/VariousTypeDataInput/variousTypeDataInput.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    int x;
+    printf("Enter Any Decimal Number: ");
+    scanf("%d", &x);
+    printf("You Ender %d", x);
+
+    float y;
+    printf("Enter Any Floating Number: ");
+    scanf("%f", &y);
+    printf("You Enter %0.2f", y);
+
+
+    return 0;
+}
